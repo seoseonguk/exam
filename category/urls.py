e8bd8668c6876618b02ae841548a7da5eccb8001
Seonguk Seo <su.seo@bakecompany.net>
2016-06-08 14:56:16 +0900

    index page category list view 및 링크 연결

--- category/urls.py
+++ category/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from .views import *
 
 urlpatterns = [
-    url(r'^$', index)
+    url(r'^$', index, name='home'),
+    url(r'^category/(?P<pk>\d+)/$', category_detail, name='category_detail')
 ]
--- category/views.py
+++ category/views.py
@@ -1,5 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Category, Shop
+
+
 
 def index(request):
+    category_list = Category.objects.all()
+    context = {
+        'category_list': category_list,
+    }
+    return render(request, "category/index.html", context)
 
-    return render(request, "category/index.html")
+def category_detail(request, pk):
+    category = get_object_or_404(Category, pk=pk)
+    context = {
+        'category': category,
+    }
+    return render(request, "category/category_detail.html", context)
