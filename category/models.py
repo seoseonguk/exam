f16f647798a13939fa805fb2a10a51610a0fc7d5
Seonguk Seo <su.seo@bakecompany.net>
2016-06-08 14:28:28 +0900

    category 모델 생성

--- category/models.py
+++ category/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    title = models.CharField(max_length=20)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+    def __str__(self):
+        return self.title
